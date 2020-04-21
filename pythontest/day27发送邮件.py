fd1633d01800d6a7fd79cffb2e7f44fc316195da
1914866205 <1914866205@qq.com>
2020-04-22 06:49:58 +0800

    day28 日历图

--- pythontest/day27发送邮件.py
+++ pythontest/day27发送邮件.py
@@ -13,35 +13,36 @@ from email.mime.multipart import MIMEMultipart
 # 负责构造邮件头信息
 from email.header import Header
 # SMTP服务器，这里使用qq邮箱
-mail_host = 'smtp.qq.com'
+mail_host = "smtp.qq.com"
 # 发件人邮箱
-mail_sender = '1914866205@qq.com'
+mail_sender = "1914866205@qq.com"
 # 邮箱授权码
-mail_license = 'pmvaikfinyymdjai'
+# mail_license = "pmvaikfinyymdjai"
+# mail_license = "xhwitcsgzcvfeebf"
+mail_license = "qazbmquloqxwbjab"
 # 收件人邮箱，可以为多个收件人
-mail_receivers = ["1065254891@qq.com"]
+mail_receivers = ["16422802@qq.com"]
 # 构建MIMEMultipart对象代表邮件本身，可以往里面添加文本，图片，附件等
 mm = MIMEMultipart('related')
 # 邮件主题
-subject_content = """来自python程序发的邮件
-"""
+subject_content = """来自python程序发的邮件"""
 # 设置发送者，注意格式，里面邮箱为发件人邮箱
-mm['From'] = '事在人为。<1914866205@qq.com>'
+mm["From"] = "小胖<1914866205@qq.com>"
 # 设置接受者，，注意格式，里面邮箱为接受者，可以设置多个
-mm['To'] = '小号<1065254891@qq.com>'
+mm["To"] = "小火锅<16422802qq.com>"
 # 设置邮件主题
 mm['Subject'] = Header(subject_content, 'utf-8')
 # 邮件正文内容
-body_content = """大号呼救小号，收到请回答！收到请回答！"""
+body_content = """早上好"""
 # 构造文本，参数1 正文内容，参数2 文本格式， 参数3 编码方式
-message_text = MIMEText(body_content, 'plain', 'utf-8')
+message_text = MIMEText(body_content, "plain", "utf-8")
 # 向MIMEMultipart对象添加文本对象
 mm.attach(message_text)
 # 构造附件
 atta = MIMEText(open('./res/excel_demo.xlsx',
                      'rb').read(), 'base64', 'utf-8')
 # 设置附件信息
-atta['Content-Disposition'] = 'attachment;filename="excel_demo.xlsx"'
+atta["Content-Disposition"] = 'attachment;filename="excel_demo.xlsx"'
 # 添加附件到邮件信息中去
 mm.attach(atta)
 # 创建SMTP对象
