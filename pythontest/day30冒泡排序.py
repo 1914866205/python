cb23a8cb563b8f8d4b64f3e22b410d20a1f98b0c
1914866205 <1914866205@qq.com>
2020-04-23 23:41:11 +0800

    修改

--- pythontest/day30冒泡排序.py
+++ pythontest/day30冒泡排序.py
@@ -5,18 +5,36 @@
 
 def bubble_sort(items, comp=lambda x, y: x > y):
     items = items[:]
-    for i in range(len(items)-1):
+    for i in range(len(items)):
         swapped = False
-        for j in range(i, len(items)-1-i):
+        for j in range(len(items)-1-i):
             if comp(items[j], items[j+1]):
                 items[j], items[j+1] = items[j+1], items[j]
                 swapped = True
-            if not swapped:
-                break
-        return items
+        if not swapped:
+            break
+    return items
 
 
 items = [5, 13, 54, 95, 3]
 
 
 print(bubble_sort(items))
+
+
+# def bubble_sort(items, comp=lambda x, y: x > y):
+#     items = items[:]
+#     for i in range(len(items) - 1):
+#         swapped = False
+#         for j in range(i, len(items) - i - 1):
+#             if comp(items[j], items[j + 1]):
+#                 items[j], items[j + 1] = items[j + 1], items[j]
+#                 swapped = True
+#             if not swapped:
+#                 break
+#         return items
+
+
+# # if __name__ == "__main__":
+# items = [58, 28, 18, 88, 8]
+# print(bubble_sort(items))
